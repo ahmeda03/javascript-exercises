c7459d0ddda2cc49bfedbcc92d596580dfc8faff
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-08 15:26:37 -0400

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,26 @@
-const repeatString = function() {
+const repeatString = function(givenString, numOfRepetitions) {
+    /*
+    IF number of Repetitions is not negative THEN
+        INIT output string
+        FOR each iteration until number of repetitions
+            concatenate given string to output string
+        ENDFOR
+        RETURN output string
+    ELSE
+        return "ERROR"
+    ENDIF
+    */
 
+    if (numOfRepetitions >= 0) {
+        let outputString = "";
+        for (let i = 0; i < numOfRepetitions; i++) {
+            outputString += givenString;
+        }
+    
+        return outputString;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
