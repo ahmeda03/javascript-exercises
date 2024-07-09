1591620c01113a09fedc8dc86a27faf60c3bd334
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-09 19:04:08 -0400

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    /*
+    IF year is divisible by 4 AND year is NOT divisible by 100 OR
+       year is divisible by 100 AND year is divisible by 400 THEN
+            RETURN true
+    END IF
 
+    RETURN false
+    */
+
+    if ((year % 4 === 0 && year % 100 !== 0) ||
+        (year % 100 === 0 && year % 400 === 0)) {
+            return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
