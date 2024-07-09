90c2bf9286d5c0dcaac552ad006f92863f3dcf80
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-09 19:34:00 -0400

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,34 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitValue) {
+  /*
+  INIT celsius value to 0
+  CALCULATE celsius using given fahrenheit value
+  RETURN celsius value
+  */
+
+  let celsiusValue = 0;
+  celsiusValue = ((fahrenheitValue - 32) * 5) / 9;
+
+  // Rounds to 1 decimal place
+  celsiusValue = Math.round(celsiusValue * 10) / 10;
+
+  return celsiusValue;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusValue) {
+  /*
+  INIT fahrenheit value to 0
+  CALCULATE fahrenheit using given celsius value
+  RETURN fahrenheit value
+  */
+
+  let fahrenheitValue = 0;
+  // fahrenheitValue = ((celsiusValue - 32) * 5) / 9;
+  fahrenheitValue = ((celsiusValue * 9) / 5) + 32;
+
+  // Rounds to 1 decimal place
+  fahrenheitValue = Math.round(fahrenheitValue * 10) / 10;
+
+  return fahrenheitValue;
 };
 
 // Do not edit below this line
