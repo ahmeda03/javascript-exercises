4278bded3c8a888ef1433c2ea25dba44562b7456
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-09 18:00:07 -0400

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    /*
+    INIT inputArr with inputString split at the characters
+    INIT outputArr to empty array
+    FOR each character starting at the end of the array
+        append character to output array
+    END FOR
 
+    SET outputString to outputArray joined at the space characters
+    RETURN outputString
+    */
+
+    let inputArr = inputString.split("");
+    let outputArr = [];
+
+    for (let i = inputArr.length - 1; i >= 0; i--) {
+        outputArr.push(inputArr[i]);
+    }
+
+    let outputString = outputArr.join("");
+    return outputString
 };
 
 // Do not edit below this line
