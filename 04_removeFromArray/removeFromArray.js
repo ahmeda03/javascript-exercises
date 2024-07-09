cad44d6a6f023a21351f36776f0df98c9e56e64f
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-09 18:23:49 -0400

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...elementsToRemove) {
+    /*
+    FOR each element in the input array
+        FOR each element in the elements to remove array
+            IF element is equal to an element to remove
+                remove element from input array
+            END IF
+    END FOR 
+
+    RETURN input array
+    */
+
+    for (let elemIndex = 0; elemIndex < inputArray.length; elemIndex++) {
+        for (const elemToRemove of elementsToRemove) {
+            if (inputArray[elemIndex] === elemToRemove) {
+                inputArray.splice(elemIndex, 1);
+                elemIndex--;
+            }
+        } 
+        
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
