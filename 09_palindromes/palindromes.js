69dc281eb5e1a179ef355333ede65e536a42d5e8
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-16 17:29:43 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let filteredStr = str.toLowerCase().replaceAll(" ", "").split("");
+    filteredStr = filteredStr.filter((letter) => (letter !== "." && letter !== "," && 
+                                                    letter !== "?" && letter !== "!"));
+    filteredStr = filteredStr.join("");
+    reversedWord = filteredStr.split("").reverse().join("");
+    return filteredStr === reversedWord;
 };
 
 // Do not edit below this line
