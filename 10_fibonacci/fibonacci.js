e1308fd3ea8070640a1f2ddc3eb90ca42ad9fbf2
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-16 18:02:04 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    
+    let fib1 = 0, fib2 = 1;
+    for (let i = 2; i <= n; i++) {
+        let fib1Tmp = fib1;
+        fib1 = fib2;
+        fib2 = fib2 + fib1Tmp;
+    }
+    return fib2;
 };
 
 // Do not edit below this line
